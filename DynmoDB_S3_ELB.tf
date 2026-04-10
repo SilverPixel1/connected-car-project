9a31ba524a15c5c2b934ca2b27a397d2bfc66a00
“SilverPixel1“ <info@projectsilverpixel.com>
2026-04-10 21:59:12 +0200

    create all tf files for the project, including ECR, ECS, Fargate(EC2), DynamoDB, ELB, S3, CloudWatch, IAM

---
+++ DynmoDB_S3_ELB.tf
@@ -0,0 +1,8 @@
+#######################################################################################################################
+#Build the Infrastructure for the Project: DynamoDB, ELB, S3
+#######################################################################################################################
+
+# DynamoDB ist die NoSQL Datenbank von AWS (schnell, skalierbar, serverless)
+# ELB ist der Elastic Load Balancer von AWS (verteilt den Traffic auf mehrere Instanzen, um hohe Verfügbarkeit zu gewährleisten)
+# S3 ist der Object Storage Service von AWS (speichert Dateien, Bilder, Videos, etc.)
+
---
+++ cloudwatch_IAM.tf
@@ -0,0 +1,7 @@
+#######################################################################################################################
+#Build the Infrastructure for the Project: CloudWatch, IAM
+#######################################################################################################################
+
+# CloudWatch ist der Monitoring Service von AWS (überwacht die Infrastruktur, sammelt Logs, erstellt Alarme, etc.)
+# IAM ist der Identity and Access Management Service von AWS (verwalten von Benutzern, Rollen, Berechtigungen, etc.)  
+
